3fa7fda407630f2605ba36322ca08efdbfb9f265
Carlos Duvan Aguirre Rodriguez <144087123+charleetrion@users.noreply.github.com>
2025-01-05 21:32:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, session
 
 # Crear instancia de FastAPI
 app = FastAPI()
